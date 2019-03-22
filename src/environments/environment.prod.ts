ba33a8b9d48b9e1216d13e35e0d47d779589192e
Manuel Espinoza <manuel.espinoza1996@gmail.com>
2019-03-21 21:31:18 -0400

    Cambios para el deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  BASE_URL: 'https://synergy.vision/marketsdata-admin/',
+  BASE_URL: '/marketsdata-admin/',
   firebase: {
     apiKey: 'AIzaSyCV6OUPuTWfTsOEf4aqpmV30swelOmelD0',
     authDomain: 'marketsdata-vision.firebaseapp.com',
